d0c513d75afeed2ee59ed78ce2800e79c9e2c5de
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2023-11-26 11:05:16 +0530

    added the post api and delete api calls

--- src/main/java/com/gl/consumerservice/ConsumerServiceApplication.java
+++ src/main/java/com/gl/consumerservice/ConsumerServiceApplication.java
@@ -3,12 +3,20 @@ package com.gl.consumerservice;
 import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @SpringBootApplication
 public class ConsumerServiceApplication {
@@ -29,26 +37,25 @@ class ConsumerController {
                 .get()
                 .retrieve()
                 .body(String.class);
-
         return emp;
     }
 
     @GetMapping("/allEmpObject/{id}")
     public Emp allEmpObject(@PathVariable long id) {
-        Emp emp = RestClient
-                .create("http://localhost:8080/emps/" + id)
+        return RestClient
+                .create()
                 .get()
+                .uri("http://localhost:8080/emps/{id}", id)
                 .retrieve()
                 .body(Emp.class);
-
-        return emp;
     }
 
     @GetMapping("/allEmps")
     public Object[] consumeAllEmps() {
         var allEmps = RestClient
-                .create("http://localhost:8080/emps")
+                .create()
                 .get()
+                .uri("http://localhost:8080/emps")
                 .retrieve()
                 .body(List.class);
 
@@ -58,8 +65,9 @@ class ConsumerController {
     @GetMapping("/allDepts")
     public Object[] allDepts() {
         var allDepts = RestClient
-                .create("http://localhost:8081/depts")
+                .create()
                 .get()
+                .uri("http://localhost:8081/depts")
                 .retrieve()
                 .body(List.class);
 
@@ -109,6 +117,49 @@ class ConsumerController {
 
         return new Object[]{allEmps, allDepts};
     }
+
+    @PostMapping("/createNewEmp")
+    public ResponseEntity<Emp> create(@RequestBody Emp emp) {
+        return RestClient
+                .create()
+                .post()
+                .uri("http://localhost:8080/emps")
+                .contentType(APPLICATION_JSON)
+                .body(emp)
+                .retrieve()
+                .toEntity(Emp.class);
+
+    }
+
+    @GetMapping("/create10Emp")
+    public void create10() {
+        IntStream.rangeClosed(1, 10)
+                .forEach(i -> {
+                    Emp emp = new Emp();
+                    emp.setName("DummyName_" + i);
+                    RestClient
+                            .create()
+                            .post()
+                            .uri("http://localhost:8080/emps")
+                            .contentType(APPLICATION_JSON)
+                            .body(emp)
+                            .retrieve()
+                            .toEntity(Emp.class);
+                });
+
+    }
+
+    @DeleteMapping("/deleteEmp")
+    public ResponseEntity<Void> delete(@RequestParam long id) {
+        return RestClient
+                .create()
+                .delete()
+                .uri("http://localhost:8080/emps/{id}", id)
+                .retrieve()
+                .toBodilessEntity();
+
+    }
+
 }
 
 @Data
